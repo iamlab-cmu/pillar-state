3f1db204fa6af17ca039c465c75e95ceb61e1f04
Timothy Lee <tel1@andrew.cmu.edu>
2020-06-13 20:06:43 -0400

    Added clean option to make_pillar_state

--- make_scripts/make_pillar_state.sh
+++ make_scripts/make_pillar_state.sh
@@ -1,5 +1,34 @@
-# TODO: add --clean option
+# Script variables
+clean_build_dir=
+
+# http://linuxcommand.org/lc3_wss0120.php
+usage()
+{
+    echo "usage: make_pillar_state [[-c] | [-h]]"
+    echo " -c | --clean: removes build directory before building"
+}
+
+# Parse input arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        -c | --clean )          shift
+                                clean_build_dir=1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
 cd pillar_state
+
+# Remove directory if we clean first
+if [ "$clean_build_dir" = "1" ]; then
+    rm -rf build
+fi
 
 # Build
 mkdir -p build && cd build
