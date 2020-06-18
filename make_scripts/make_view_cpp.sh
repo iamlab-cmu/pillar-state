b3a49e0b297a524ed7476153c1c8e59647b5b1ed
Timothy Lee <tel1@andrew.cmu.edu>
2020-06-18 18:47:29 -0400

    Forgot to add sh scripts

---
+++ make_scripts/make_view_cpp.sh
@@ -0,0 +1,38 @@
+# Script variables
+clean_build_dir=
+
+# http://linuxcommand.org/lc3_wss0120.php
+usage()
+{
+    echo "usage: make_pillar_state [[-c] | [-h]]"
+    echo " -c | --clean: removes build directory before building"
+}
+
+# Parse input arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        -c | --clean )          shift
+                                clean_build_dir=1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+cd cpp
+
+# Remove directory if we clean first
+if [ "$clean_build_dir" = "1" ]; then
+    rm -rf build
+fi
+
+# Build
+mkdir -p build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config -- -j$nproc
+
+cd ../..
