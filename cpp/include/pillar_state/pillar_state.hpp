5f223a04b44b11d1ca0a1ee7aa69f336ce9f95c3
Timothy Lee <tel1@andrew.cmu.edu>
2020-06-23 13:49:45 -0400

    Minor updates for testing out protobuf interface

--- cpp/include/pillar_state/pillar_state.hpp
+++ cpp/include/pillar_state/pillar_state.hpp
@@ -16,10 +16,37 @@ public:
   {
     std::cout << "Hello there." << std::endl;
   }
-  bool has_high_ground()
+  bool has_high_ground() const
   {
     std::cout << "It's over, Anakin." << std::endl;
     return true;
+  }
+  void add_property_value_test(const std::string& property_name, double property_value)
+  {
+    std::cout << "add_property_test\n" << property_name << std::endl << property_value << std::endl;
+    PillarMsg::Property property_test;
+    std::cout << "property_test.value_size(): " << property_test.value_size() << std::endl;
+    std::cout << "property_test.variance_size(): " << property_test.variance_size() << std::endl;
+    property_test.add_value(property_value);
+    std::cout << "property_test.value_size(): " << property_test.value_size() << std::endl;
+    std::cout << "property_test.variance_size(): " << property_test.variance_size() << std::endl;
+  }
+  void add_property(const std::string& property_name, const PillarMsg::Property& property)
+  {
+    //(*state_)[property_name] = property;
+    //state_.properties[property_name] = property;
+    (*state_.mutable_properties())[property_name] = property;
+  }
+  void add_property(const std::string& property_name, double value)
+  {
+    // scalar version
+    PillarMsg::Property property;
+    property.add_value(value);
+    add_property(property_name, property);
+  }
+  int num_properties() const
+  {
+    return state_.properties().size();
   }
 private:
   PillarMsg::Property property_;
--- test/pillar_state_test.cpp
+++ test/pillar_state_test.cpp
@@ -3,6 +3,8 @@
 
 #include "pillar_state/pillar_state.hpp"
 
+#include "pillar_state/proto/pillar_state.pb.h"
+
 // using namespace pillar_state;
 // using namespace Eigen;
 
@@ -15,4 +17,14 @@ TEST(PillarState, Nominal)
 {
   Pillar::State state;
   EXPECT_TRUE(state.has_high_ground());
+
+  state.add_property_value_test("general", 66.);
+  state.add_property_value_test("kenobi", 66.);
+
+  PillarMsg::Property property;
+  property.add_value(1.111);
+  property.add_value(2.222);
+  property.add_value(3.333);
+  state.add_property("array", property);
+  state.add_property("scalar", 4.444);
 }
