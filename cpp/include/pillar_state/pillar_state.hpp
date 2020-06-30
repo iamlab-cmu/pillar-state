ee8837f6894309b63710baa62feadf4f63c84cac
Timothy Lee <tel1@andrew.cmu.edu>
2020-06-30 10:32:33 -0400

    Removed test code from Pillar::State

--- cpp/include/pillar_state/pillar_state.hpp
+++ cpp/include/pillar_state/pillar_state.hpp
@@ -12,25 +12,7 @@ namespace Pillar
 class State
 {
 public:
-  State()
-  {
-    std::cout << "Hello there." << std::endl;
-  }
-  bool has_high_ground() const
-  {
-    std::cout << "It's over, Anakin." << std::endl;
-    return true;
-  }
-  void add_property_value_test(const std::string& property_name, double property_value)
-  {
-    std::cout << "add_property_test\n" << property_name << std::endl << property_value << std::endl;
-    PillarMsg::Property property_test;
-    std::cout << "property_test.value_size(): " << property_test.value_size() << std::endl;
-    std::cout << "property_test.variance_size(): " << property_test.variance_size() << std::endl;
-    property_test.add_value(property_value);
-    std::cout << "property_test.value_size(): " << property_test.value_size() << std::endl;
-    std::cout << "property_test.variance_size(): " << property_test.variance_size() << std::endl;
-  }
+  State() { }
   void add_property(const std::string& property_name, const PillarMsg::Property& property)
   {
     //(*state_)[property_name] = property;
@@ -94,11 +76,9 @@ public:
       os << "n/a" << std::endl;
     }
 
-    // return os << "hay\n";
     return os;
   }
 private:
-  PillarMsg::Property property_;
   PillarMsg::State state_;
 };
 
