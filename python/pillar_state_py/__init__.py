b26df096fd6cf9f187afb96e63866da4ec5a614d
Timothy Lee <tel1@andrew.cmu.edu>
2020-12-08 22:41:00 -0500

    Added verion information for python bindings for PILLAR State

--- python/pillar_state_py/__init__.py
+++ python/pillar_state_py/__init__.py
@@ -1,1 +1,3 @@
+__version__ = "0.0.0"
+
 from .pillar_state import State
--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,7 @@
-import sys
+import io
 import os
+import re
+import sys
 
 from setuptools import setup, find_packages
 
@@ -10,12 +12,20 @@ def collect_files(target_dir):
             file_list.append(os.path.join('..', root, filename))
     return file_list
 
+# This method was adapted from code in
+#  https://github.com/albumentations-team/albumentations
+def get_version():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    version_file = os.path.join(current_dir, "pillar_state_py", "__init__.py")
+    with io.open(version_file, encoding="utf-8") as f:
+        return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
+
 def _do_setup():
     packages = find_packages('.')
     package_files = collect_files('pillar_state_py/_bindings/linux-x86_64')
 
     setup(name='pillar_state_py',
-          version='0.0.0',
+          version=get_version(),
           author='Timothy Lee, Jacky Liang',
           author_email='tel1@andrew.cmu.edu',
           description='Library for state space modeling with PILLAR.',
